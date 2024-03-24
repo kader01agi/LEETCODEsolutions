50482689bcf21a17b4e8248afe947d49c44cfc3b
kader01agi <md.abdul.kader.ru@gmail.com>
2024-03-24 14:20:00 +0600

    Leetcode 58 solution

--- PYTHON/STRING/58.py
+++ PYTHON/STRING/58.py
@@ -2,7 +2,7 @@ def lengthOfLastWord(s: str) -> int:
     """
     Given a string s consisting of words and spaces, return the length of the last word in the string.
     A word is a maximal substring  consisting of non-space characters only.
-""" 
+    """ 
     s = s.strip()
     strList = s.split(" ")
     print(strList)
