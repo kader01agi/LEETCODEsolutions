0dac8f8eab7a4626ec869ae3132f05b2f228f546
kader01agi <md.abdul.kader.ru@gmail.com>
2024-03-24 14:11:25 +0600

    Leetcode 58 solution

---
+++ PYTHON/STRING/58.py
@@ -0,0 +1,13 @@
+def lengthOfLastWord(s: str) -> int:
+    """
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+    A word is a maximal substring  consisting of non-space characters only.
+""" 
+    s = s.strip()
+    strList = s.split(" ")
+    print(strList)
+    return len(strList[-1])
+
+s = "Hello World"
+s1 = "   fly me   to   the moon  "
+print(lengthOfLastWord(s1))
